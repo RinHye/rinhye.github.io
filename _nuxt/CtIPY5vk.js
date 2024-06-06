e5e338dbcd9d526dfb9b5555eec17bcbb0016c14
RinHye <pauline@DESKTOP-8UVJE31>
2024-06-06 23:11:47 +0200

    Fix warnings - Path to Logo.png - Default value of the card width in projects

---
+++ _nuxt/CtIPY5vk.js
@@ -0,0 +1,1 @@
+import{p as s}from"./JsTALKrT.js";const o=s("/images/projets/Alpiq.jpg");export{o as _};
---
+++ _nuxt/Dl_rykkJ.js
@@ -0,0 +1,1 @@
+import"./JsTALKrT.js";const r=""+new URL("phone-screen.CT5QqU-P.png",import.meta.url).href;export{r as _};
